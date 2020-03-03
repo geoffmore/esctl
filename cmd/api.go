108158bc30842a70fae311e59656a58140bfb2e6
Jeff Moore <jeoffmore@gmail.com>
2020-03-03 13:51:17 -0500

    Commit more incomplete code to show progress

--- cmd/api.go
+++ cmd/api.go
@@ -10,39 +10,52 @@ import (
 
 func init() {
 	rootCmd.AddCommand(apiCmd)
-	apiCmd.AddCommand(apiTest)
-	apiCmd.AddCommand(apiInt)
+	//apiCmd.AddCommand(apiTest)
+	//apiCmd.AddCommand(apiInt)
 }
 
 var apiCmd = &cobra.Command{
 	// esctl api
 	Use:   "api",
 	Short: "No description",
-}
-
-var apiTest = &cobra.Command{
-	// esctl api test
-	Use:   "test-cmd",
-	Short: "Generated code to demonstrate project structure",
-	Run: func(cmd *cobra.Command, args []string) {
-		api.CmdTest()
-	},
-}
-
-var apiInt = &cobra.Command{
-	// esctl api test
-	Use:   "test-int",
-	Short: "Generated code to demonstrate project structure",
+	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		// Boilerplate
 		client, err := genClient()
 		if err != nil {
 			log.Fatal(err)
 		}
-
-		err = api.IntegrationTest(client)
+		// I don't like the function name, but it'll have to... _Do_
+		err = api.Do(client, args[0], args[1])
 		if err != nil {
 			log.Fatal(err)
 		}
 	},
 }
+
+//var apiTest = &cobra.Command{
+//	// esctl api test
+//	Use:   "test-cmd",
+//	Short: "Generated code to demonstrate project structure",
+//	Run: func(cmd *cobra.Command, args []string) {
+//		api.CmdTest()
+//	},
+//}
+//
+//var apiInt = &cobra.Command{
+//	// esctl api test
+//	Use:   "test-int",
+//	Short: "Generated code to demonstrate project structure",
+//	Run: func(cmd *cobra.Command, args []string) {
+//		// Boilerplate
+//		client, err := genClient()
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//
+//		err = api.IntegrationTest(client)
+//		if err != nil {
+//			log.Fatal(err)
+//		}
+//	},
+//}
--- pkg/api/api.go
+++ pkg/api/api.go
@@ -4,41 +4,83 @@ package api
 
 import (
 	"fmt"
-	elastic7 "github.com/elastic/go-elasticsearch/v7"
-	"github.com/elastic/go-elasticsearch/v7/esapi"
-	"github.com/geoffmore/esctl-go/esutil"
-	"net/http"
+	//elastic7 "github.com/elastic/go-elasticsearch/v7"
+	//"github.com/elastic/go-elasticsearch/v7/esapi"
+	//"github.com/geoffmore/esctl-go/esutil"
+	//"net/http"
+	"context"
 )
 
 func CmdTest() {
 	fmt.Println("Test")
 }
 
-func IntegrationTest(esClient *elastic7.Client) error {
-	req := esapi.ClusterHealthRequest{
-		Pretty: true,
-		Human:  true,
-	}
+//func IntegrationTest(esClient *elastic7.Client) error {
+//	req := esapi.ClusterHealthRequest{
+//		Pretty: true,
+//		Human:  true,
+//	}
+//
+//	err := esutil.Request(req, esClient)
+//	return err
+//}
 
-	err := esutil.Request(req, esClient)
-	return err
+//func buildRequest(esClient *elastic7.Client, method string, endpoint string) (req *http.Request, err error) {
+//	// https://pkg.go.dev/net/http?tab=doc#Request
+//
+//	// Build complete URL
+//	// Validate and add method
+//	// Add optional body? This needs to be supported by a flag. Maybe -d?
+//	// var req http.Request
+//
+//	// I should probably use IsError() syntax eventually
+//	req, err = http.NewRequest(method, url)
+//
+//	// Add auth?
+//
+//	// Validate and add URL from esClient
+//
+//	return req, err
+//}
+
+// Do I pass this up? For now, I say yes
+//func makeRequest(esClient *elastic7.Client, req *http.Request) (response *http.Response, err error) {
+//	//	// func (c *Client) Perform(req *http.Request) (*http.Response, error)
+//	//	res, err = esClient.Perform(req)
+//	//	return response, err
+//}
+func Do(esClient *elastic7.Client, method string, endpoint string) error {
+	// Assumes absolutely no errors
+	req, err = buildRequest(esClient, method, endpoint)
+	res, err = makeRequest(esClient, req)
+
+	// I need:
+	//type esRequest interface {
+	//	Do(context.Context, esapi.Transport) (*esapi.Response, error)
+	//}
+	// I should find out how to use esutil, but that has its complexities. I
+	// think it boils down to satisfying this interface: https://godoc.org/github.com/elastic/go-elasticsearch/esapi#Transport
+
+	// Transport interface -> esRequest interface
 }
 
-func buildRequest(esClient *elastic7.Client, method string, endpoint string) (req *http.Request, err error) {
-	// https://pkg.go.dev/net/http?tab=doc#Request
-
-	// Build complete URL
-	// Validate and add method
-	// Add optional body? This needs to be supported by a flag. Maybe -d?
-	// var req http.Request
-
-	// I should probably use IsError() syntax eventually
-	req, err = http.NewRequest(method, url)
-
-	// Add auth?
-
-	// Validate and add URL from esClient
-
-	return req, err
+type apiFooTransport struct {
 }
-func makeRequest(esClient *elastic7.Client) (response *http.Response, err error) {}
+
+// This is now a esapi.Transport
+func (a apiFooTransport) Perform(*http.Request) (*http.Response, error) {
+}
+
+type apiFooEsRequest struct{}
+
+// This is now esRequest
+func (a apiFooEsRequest) Do(context.Context, esapi.Transport) (*esapi.Response, error)
+
+// This is esapi.Response
+// type Response struct {
+//    StatusCode int
+//    Header     http.Header
+//    Body       io.ReadCloser
+//}
+// What if each subcommand of the api subcommand was a http verb?
+// I don't like that idea. Too much work
