ec1a38ffa490313b129e2886639370238e6bbbd0
Jeff Moore <jeoffmore@gmail.com>
2020-03-06 23:51:00 -0500

    Added tooling to bump versions

--- tools/bump-version.sh
+++ tools/bump-version.sh
@@ -10,41 +10,50 @@ semver_regex+='(\+([0-9A-Za-z\-]+(\.[0-9A-Za-z\-]+)*))?'
 
 version_file="pkg/version/version.go"
 
-
 ## Get the current version from pkg/version/version.go
 # (Is this the correct source of tags?)
 # https://stackoverflow.com/questions/1064499
 # BASH regex doesn't work for now. grep does
-version=$(grep -Eo "${semver_regex}" ${version_file})
-echo ${version}
-#if [[ ${version_file} =~ $semver_regex ]]; then
-#  version="${BASH_REMATCH[1]}"
-#  
-#  echo ${version}
-#fi
+old_version=$(grep -Eo "${semver_regex}" ${version_file})
+
+echo "Old version: ${old_version}"
 
 # Determine semver operation. Fail on garbage input
 # Options are minor and patch
 case ${1} in
   "minor")
-    semver_op="minor"
+    semver_index=1
     ;;
   "patch")
-    semver_op="patch"
+    semver_index=2
     ;;
   *)
     echo "Invalid input. Use argument 'minor' or 'patch'"
     exit 1
 esac
 
+# Split the semver string into an array
+# https://askubuntu.com/questions/89995
+# https://stackoverflow.com/questions/918886
+IFS='.'; read -ra version_arr <<< "${old_version#"v"}"
 
-# minor
-# patch
+# Bump part of semver based on user input
+let version_arr[${semver_index}]++
+# Join the semver array back into a string
+# https://superuser.com/questions/461981
+new_version="v$(IFS='.'; echo "${version_arr[*]}")"
+
+echo "New version: ${new_version}"
 
 ## Bump version in pkg/version/version.go
-
-## Bump version in go.mod?
+#-i when I'm ready
+sed  -i "s/$old_version/$new_version/g" "${version_file}"
 
 ## Make an empty commit "bump version to {new_version}"
-#git commit --allow-empty -m "Bump version to ${NEW_VERSION}"
-#git tag "${NEW_VERSION}"
+git commit --allow-empty -m "Bump version ${old_version} -> ${new_version}"
+git tag "${new_version}"
+
+## Bump version in go.mod?
+# go get new tag
+# replace line in go.mod
+# go mod tidy(?)
